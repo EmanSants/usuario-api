2b498d94efef2a8b44f4c0cd6be861168926ef9e
EmanSants <emansantsilv@gmail.com>
2026-09-24 20:34:44 -0300

    adicionadas classes de security

---
+++ src/main/java/com/emanuel/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.emanuel.usuario.infrastructure.repository;
+
+import com.emanuel.usuario.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import jakarta.transaction.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
